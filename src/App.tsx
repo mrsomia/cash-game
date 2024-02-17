e3d55740bbcf404c1d12bf79de0474749af6d629
Sachin Somia <sachinsomia@gmail.com>
2024-02-17 14:21:58 +0000

    feat: Adds list of Players via solid store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import { createSignal } from "solid-js";
+import { For, Component } from "solid-js";
 import "./App.css";
+import { createStore } from "solid-js/store";
 
 function App() {
-  const [count, setCount] = createSignal(0);
+  const [players, setPlayers] = createStore([
+    {
+      name: "Player 1",
+      buyin: 0,
+      end: 0,
+    },
+    {
+      name: "Player 2",
+      buyin: 0,
+      end: 0,
+    },
+  ]);
 
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div class="container bg-slate-900 w-100 min-h-screen text-white">
+      <h1 class="py-12 text-2xl lg:text-3xl text-center font-bold">
+        Cash Game
+      </h1>
+      <div class="w-10/12 mx-auto">
+        <For each={players}>{(player) => <Player player={player} />}</For>
+      </div>
+    </div>
+  );
 }
+
+type PlayerValues = {
+  name: string;
+  buyin: number;
+  end: number;
+};
+
+const Player: Component<{ player: PlayerValues }> = (props) => {
+  return (
+    <div class="w-100 flex justify-between">
+      <div class="block">{props.player.name}</div>
+      <div class="block">{props.player.buyin.toFixed(2)}</div>
+      <div class="block">{props.player.end.toFixed(2)}</div>
+    </div>
+  );
+};
 
 export default App;

948cd90b19cf998945a75b1ce8568a6e33fc241a
Sachin Somia <sachinsomia@gmail.com>
2024-03-07 19:19:03 +0000

    feat(app): initial rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { For, Component } from "solid-js";
+import { For, Component, createSignal } from "solid-js";
 import "./App.css";
 import { createStore } from "solid-js/store";
 
 function App() {
-  const [players, setPlayers] = createStore([
+  const [players, _setPlayers] = createStore([
     {
       name: "Player 1",
       buyin: 0,
@@ -16,13 +16,42 @@ function App() {
     },
   ]);
 
+  const [openRow, setOpenRow] = createSignal<null | number>(null);
+
+  const handleToggleRow = (index: number) => {
+    if (openRow() == index) {
+      setOpenRow(null);
+    } else {
+      setOpenRow(index);
+    }
+    console.log({ open: openRow() });
+  };
+
   return (
-    <div class="container bg-slate-900 w-100 min-h-screen text-white">
-      <h1 class="py-12 text-2xl lg:text-3xl text-center font-bold">
-        Cash Game
-      </h1>
-      <div class="w-10/12 mx-auto">
-        <For each={players}>{(player) => <Player player={player} />}</For>
+    <div class="bg-slate-900 w-100 min-h-screen text-white">
+      <div class="container">
+        <h1 class="py-12 text-2xl lg:text-3xl text-center font-bold">
+          Cash Game
+        </h1>
+        <div class="w-10/12 mx-auto text-center">
+          <div class="w-100 flex justify-between py-4 px-2">
+            <div>Name</div>
+            <div class="pl-4">Buy in</div>
+            <div>End stack</div>
+            <div>
+              <span> </span>
+            </div>
+          </div>
+          <For each={players}>
+            {(player, index) => (
+              <Player
+                player={player}
+                open={index() == openRow()}
+                setOpenRow={() => handleToggleRow(index())}
+              />
+            )}
+          </For>
+        </div>
       </div>
     </div>
   );
@@ -34,12 +63,17 @@ type PlayerValues = {
   end: number;
 };
 
-const Player: Component<{ player: PlayerValues }> = (props) => {
+const Player: Component<{
+  player: PlayerValues;
+  open: boolean;
+  setOpenRow: () => void;
+}> = (props) => {
   return (
     <div class="w-100 flex justify-between">
-      <div class="block">{props.player.name}</div>
-      <div class="block">{props.player.buyin.toFixed(2)}</div>
-      <div class="block">{props.player.end.toFixed(2)}</div>
+      <div>{props.player.name}</div>
+      <div>{props.player.buyin.toFixed(2)}</div>
+      <div>{props.player.end.toFixed(2)}</div>
+      <div onClick={props.setOpenRow}>+</div>
     </div>
   );
 };
